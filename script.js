bf7e82caa43bf8ab0c786ec5ddf77c2616117971
lucas mendes <lucasmndes.dev@gmail.com>
2024-11-18 15:55:32 -0300

    primeira parte do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("botao")
+const btn2 = document.getElementById("botao2")
+ 
+btn.addEventListener("click", ()=> {
+   btn.classList.toggle("active")
+})
+
+btn2.addEventListener("click", ()=> {
+    btn2.classList.toggle("active")
+ })
+
