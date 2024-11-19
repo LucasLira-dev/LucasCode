95c92e83bd9064ac8f27293a2e1afd7824261a9e
lucas mendes <lucasmndes.dev@gmail.com>
2024-11-19 14:59:00 -0300

    finalização do site

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const btn = document.getElementById("botao")
+const btn = document.getElementById('botao')
 const btn2 = document.getElementById("botao2")
+const btn3 = document.getElementById("botao3")
  
 btn.addEventListener("click", ()=> {
    btn.classList.toggle("active")
@@ -9,3 +10,31 @@ btn2.addEventListener("click", ()=> {
     btn2.classList.toggle("active")
  })
 
+ btn3.addEventListener("click", ()=> {
+   btn3.classList.toggle("active")
+})
+
+
+
+
+
+    let inputElement = document.getElementById("pesquisa");   
+    
+    let sectionElements = document.querySelectorAll("section");
+
+    let noresult = document.getElementById("semResultado")
+
+     inputElement.addEventListener("input", (e) => 
+      { let inputed = e.target.value.toLowerCase(); 
+          sectionElements.forEach((section) => {
+             let h1 = section.querySelector("h1"); 
+              if (h1 && h1.textContent.toLowerCase().includes(inputed))
+                { 
+                   section.style.display = "block"; 
+
+             } else {
+                section.style.display = "none"; 
+               
+             } 
+            });
+          });
