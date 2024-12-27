8bde27b857b41f68886e1d8845c53dd65ba00225
lucas mendes <lucasmndes.dev@gmail.com>
2024-12-27 15:00:54 -0300

    barra de pesquisas aceitando sem acentos

--- script.js
+++ script.js
@@ -11,37 +11,31 @@ const btn = [...document.querySelectorAll('.botao')]
 
 
  
+ document.addEventListener("DOMContentLoaded", () => {
+  let inputElement = document.getElementById("pesquisa");   
+  let sectionElements = document.querySelectorAll("section");
+  let noresult = document.getElementById("semResultado");
+  let footer = document.getElementById("footer");
 
+  inputElement.addEventListener("input", (e) => {
+    let inputed = e.target.value.toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    let found = false;
+    sectionElements.forEach((section) => {
+      let h1 = section.querySelector("h2");
+      if (h1 && h1.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(inputed)) {
+        section.style.display = "block";
+        found = true;
+      } else {
+        section.style.display = "none";
+      }
 
-    let inputElement = document.getElementById("pesquisa");   
-    
-    let sectionElements = document.querySelectorAll("section");
-
-    let noresult = document.getElementById("semResultado")
-
-    let footer = document.getElementById("footer")
-
-     inputElement.addEventListener("input", (e) => 
-      { let inputed = e.target.value.toLowerCase().trim()
-        let found = false
-          sectionElements.forEach((section) => {
-             let h1 = section.querySelector("h2"); 
-              if (h1 &&  h1.textContent.toLowerCase().includes(inputed))
-                { 
-                   section.style.display = "block"; 
-                   found = true
-               
-             } else  {
-                  section.style.display = "none"; 
-                 
-             } 
-
-             if (inputed === ""){
-               noresult.style.display = "none"
-               footer.classList.remove('fundo')
-             } else {
-               noresult.style.display = found ? "none" : "block"
-                footer.classList.add('fundo')
-             }
-            });
-          });
+      if (inputed === "") {
+        noresult.style.display = "none";
+        footer.classList.remove('fundo');
+      } else {
+        noresult.style.display = found ? "none" : "block";
+        footer.classList.add('fundo');
+      }
+    });
+  });
+});
